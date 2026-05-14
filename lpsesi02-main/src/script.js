8cc87d3ef4255a274d8933eaead29e43f821b904
Masko <maskosbreak@gmail.com>
2026-05-14 07:57:48 -0300

    Add files via upload

---
+++ lpsesi02-main/src/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', ()=> {
+    const nav = document.querySelector('nav')
+
+    if(window.scrollY > 0){
+        nav.classList.add('sc')
+        footer.classList.add('ft')
+    }else{
+        nav.classList.remove('sc')
+        footer.classList.remove('ft')
+    }
+})
+function navTo(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
